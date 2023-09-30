8cce5f82e4ebe1c163ceecb4322caaecdd2f26ca
Cláudio <claudioalvesoficial@gmail.com>
2023-09-30 16:09:09 -0300

    Dropdown cities

--- TripHelperApp/static/index.js
+++ TripHelperApp/static/index.js
@@ -1,9 +1,22 @@
 const select = document.querySelector("#countrySelect");
+const selectCity = document.querySelector("#countrySelectCity")
 
-select.addEventListener("change", (e) => {
-    console.log(e.target.value)
-})
+// async function fetchCitiesData(country) {
+//     try {
+//       const response = await fetch(`api/${country}/cities`);
+      
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+      
+//       const data = await response.json();
+//       return data;
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
 
-function findCities(country) {
-    fetch(`api/${country}/cities`).then(text => console.log(text))
-}
+// select.addEventListener("change", (e) => {
+//     console.log(fetchCitiesData(e.target.value))
+//     selectCity.style.display = "block"
+// })
