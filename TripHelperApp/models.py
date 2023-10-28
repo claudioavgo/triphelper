a42a57ab35f1436cce86d0fc3df30e381c21d571
GabrielLimaSC <gabrielsiqueira311@gmail.com>
2023-10-28 17:05:50 -0300

    function like and dislike

--- TripHelperApp/models.py
+++ TripHelperApp/models.py
@@ -9,3 +9,15 @@ class Perfil(models.Model):
 
     def __str__(self):
         return self.usuario.username
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title 
+    
+    def increase_likes(self):
+        self.likes += 1
+        self.save()
