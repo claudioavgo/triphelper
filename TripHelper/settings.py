d530c8e34b63668742047afa733718e7dd359370
Artur Souto <artursmpm@gmail.com>
2023-10-30 03:36:02 -0300

    tests

--- TripHelper/settings.py
+++ TripHelper/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'TripHelper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'th',
+        'USER': 'adminth',
+        'PASSWORD': 'ADWaATH123!',
+        'HOST': 'triphelperdb.postgres.database.azure.com', 
+        'PORT': '5432',
     }
 }
 
---
+++ tests/testfav.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+time.sleep(5)
